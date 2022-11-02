a7eb9e4414164f5f92f8b1598e0bd9ec6ebed424
tiennv-comartek <tiennv@comartek.com>
2022-11-02 17:11:43 +0700

    fix login

--- src/modules/Auth/router.ts
+++ src/modules/Auth/router.ts
@@ -1,6 +1,6 @@
 import { CommonPath } from '@commons/base-routes';
 import LoginPage from '@modules/Auth/pages/Login';
-import checkLogin from './hoc/checkLogin';
+import checkLogin from '../../hoc/checkLogin';
 
 export const AuthRouter = {
   path: CommonPath.LOGIN_PATH,
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import EmptyLayout from '@commons/components/layouts/Empty';
 import MasterLayout from '@commons/components/layouts/MasterLayout';
-import checkLogin from '@modules/Auth/hoc/checkLogin';
+import checkLogin from '@hoc/checkLogin';
 import { AuthRouter } from '@modules/Auth/router';
 import { HomeRouter } from '@modules/Home/router';
 import { RouteConfig } from 'react-router-config';
